c5f6247e33acc86e0bc7c4f280e14d2d902bb7f9
skhe55 <flashback2019@mail.ru>
2021-10-12 17:17:51 +0700

    added router, switch & etc..

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './Store';
+import Main from './Components/Main';
 import 'styles/global.scss';
-const App = () => {
+
+
+export const App: React.FC<{}> = () => {
+    // const [photos, setPhotos] = useState<any>([]);
+    // async function getData() {
+    //     const url: string = 'https://jsonplaceholder.typicode.com/photos?_limit=10';
+    //     const data: any = await fetch(url);
+    //     const json: any = await data.json();
+    //     setPhotos(json);
+    // };
+    // getData();
+
+    // useEffect(() => {
+    //     document.addEventListener('scroll', scrollHandler)
+    //     return function () {
+    //         document.removeEventListener('scroll', scrollHandler)
+    //     }
+    // }, [])
+    // const scrollHandler = (e) => {
+    //     console.log('scroll');
+    // }
+    // return (
+    //     <div className={'main'}>
+    //         {photos.map((photo: any) =>
+    //             <div className='photo' key={photo.id}>
+    //                 <img src={photo.thumbnailUrl}></img>
+    //             </div>
+    //         )}
+    //     </div>
+    // );
     return (
-        <div>
-            Start page
-        </div>
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Main} />
+                </Switch>
+            </Router>
+        </Provider>
     );
 }
 
