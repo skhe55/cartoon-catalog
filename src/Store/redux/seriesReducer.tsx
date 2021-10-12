b5c6d6730aa6fc3e80df816f01383302763cafd0
skhe55 <flashback2019@mail.ru>
2021-10-12 17:05:57 +0700

    series reducer

---
+++ src/Store/redux/seriesReducer.tsx
@@ -0,0 +1,22 @@
+import { SET_SERIES } from "./type";
+
+const initialState: any = {
+    series: []
+};
+
+export default function seriesReducer(state = initialState, action: any) {
+    switch (action.type) {
+        case SET_SERIES:
+            return {
+                ...state,
+                series:
+                    [
+                        ...state.series,
+                        ...action.payload
+                    ]
+            }
+        default: {
+            return state;
+        }
+    }
+};
