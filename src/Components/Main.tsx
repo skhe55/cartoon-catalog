dd58bd01e6fdc9ce700b73ae21001dbcd29b450a
skhe55 <flashback2019@mail.ru>
2021-10-12 23:56:34 +0700

    showed all series separate by seasons

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -3,13 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { FETCH_SERIES } from '../Store/redux/type';
 import '../styles/Main.scss';
 import CardItem from './SeasonCards/CardItem';
-import Cards from './SeasonCards/Cards';
+import Header from './SeasonCards/Header';
 
 function Main() {
     const series = useSelector(state => state.seriesReducer.series);
     const dispatch = useDispatch();
     const [loading, setLoading] = useState<boolean>(false);
-    const [currentSeason, setCurrentSeason] = useState<number>(1);
     const getEpisodes = (el: any) => {
         return series.filter((e: any) => e.episode.slice(2, 3) == el);
     }
@@ -23,19 +22,64 @@ function Main() {
     return (
         <div className='_mainFlexContainer'>
             <div className='_wrapperMainFlexCont'>
-                <i className="fas fa-search"></i>
+                {/* <i className="fas fa-search"></i> */}
                 <input className='_inputSearch' placeholder="Type here"></input>
             </div>
-            <Cards />
-            {seasons.map((e: any) =>
-                getEpisodes(e).map((el: any) =>
-                    <CardItem
-                        nameEP={el.name}
-                        date={el.air_date}
-                        serialnum={el.episode.slice(4, 6)}
-                    />
-                )
-            )}
+            <Header
+                season={1}
+            />
+            <div className='_wrapContainer'>
+                <div className='_rowFlexWrap'>
+                    {getEpisodes(1).map((el: any) =>
+                        <CardItem
+                            nameEP={el.name}
+                            date={el.air_date}
+                            serialnum={el.episode.slice(4, 6)}
+                        />
+                    )} </div>
+            </div>
+            <Header
+                season={2}
+            />
+            <div className='_wrapContainer'>
+                <div className='_rowFlexWrap'>
+                    {getEpisodes(2).map((el: any) =>
+                        <CardItem
+                            nameEP={el.name}
+                            date={el.air_date}
+                            serialnum={el.episode.slice(4, 6)}
+                        />
+                    )}
+                </div>
+            </div>
+            <Header
+                season={3}
+            />
+            <div className='_wrapContainer'>
+                <div className='_rowFlexWrap'>
+                    {getEpisodes(3).map((el: any) =>
+                        <CardItem
+                            nameEP={el.name}
+                            date={el.air_date}
+                            serialnum={el.episode.slice(4, 6)}
+                        />
+                    )}
+                </div>
+            </div>
+            <Header
+                season={4}
+            />
+            <div className='_wrapContainer'>
+                <div className='_rowFlexWrap'>
+                    {getEpisodes(4).map((el: any) =>
+                        <CardItem
+                            nameEP={el.name}
+                            date={el.air_date}
+                            serialnum={el.episode.slice(4, 6)}
+                        />
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
