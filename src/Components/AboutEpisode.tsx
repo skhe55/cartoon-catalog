0177b740458457bee6e04338daaa70da74958df9
skhe55 <flashback2019@mail.ru>
2021-10-15 01:47:42 +0700

    added animated loading pre-screen if data yet not loading, some fixex with rendering components

--- src/Components/AboutEpisode.tsx
+++ src/Components/AboutEpisode.tsx
@@ -1,31 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 import { useSelector, useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import { FETCH_CHARACTERS } from '../Store/redux/type';
-import { resetData } from '../Store/redux/actions';
 import { RootState } from '../Store/redux';
+import Card from './CharactersCards/Card';
+import '../styles/AboutEpisode.scss';
 
 function AboutSeries() {
     const characters = useSelector((state: RootState) => state.charactersReducer.characters);
+    const isFetching = useSelector((state: RootState) => state.charactersReducer.isFetching);
     const dispatch = useDispatch();
     const { state } = useLocation();
     useEffect(() => {
         dispatch({ type: FETCH_CHARACTERS, payload: state.charactersUrlList })
     }, []);
     console.log(characters);
+    console.log(isFetching);
     return (
         <div>
-            <NavLink to="/" onClick={() => dispatch(resetData())}>Hello</NavLink>
-            <div>
-                {characters.map((el: any) => {
-                    return (
-                        <>
-                            {el.name}
-                        </>
-                    );
-                })}
-            </div>
+            {!isFetching ? <div className="fetching"></div> :
+                <div className="_bodyFlex">
+                    {characters.map((el: any) => {
+                        return (
+                            <Card
+                                imageUrl={el.image}
+                                name={el.name}
+                                species={el.species}
+                                status={el.status}
+                                location={el.location.name}
+                            />
+                        );
+                    })}
+                </div>
+            }
         </div>
     );
 }
