7b7426a5ba36fc5af31081aa5b2833db9cfa71bd
skhe55 <flashback2019@mail.ru>
2021-10-15 01:41:22 +0700

    Create responsive navbar & added routing between pages

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
+import { resetData } from '../../Store/redux/actions';
+import { useDispatch } from 'react-redux';
+import './Navbar.scss';
+
+function Navbar() {
+    const dispatch = useDispatch();
+    const [isMobile, setIsMobile] = useState<boolean>(false);
+    return (
+        <nav>
+            <ul className="navbar">
+                <h3 className="logo">Rick and Morty</h3>
+                <img className="icon-logo" src="https://img.icons8.com/plasticine/100/000000/rick-sanchez.png" />
+                <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
+                    <NavLink to="/" className="Home" onClick={() => dispatch(resetData())}>
+                        <li>Home</li>
+                    </NavLink>
+                    <NavLink to="/" className="AboutCharacter">
+                        <li>About Character</li>
+                    </NavLink>
+                    <NavLink to="/" className="AboutLocation">
+                        <li>About Location</li>
+                    </NavLink>
+                </ul>
+                <button className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
+                    {
+                        isMobile ? <i className="fas fa-times" />
+                            : <i className="fas fa-bars" />
+                    }
+                </button>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
