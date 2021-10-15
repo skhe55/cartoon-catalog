4bc0b02c4b8d8a36e868c73598e64b61311e022f
skhe55 <flashback2019@mail.ru>
2021-10-16 01:06:05 +0700

    was add reducer&saga for characters

--- src/Store/redux-saga/charactersSaga.tsx
+++ src/Store/redux-saga/charactersSaga.tsx
@@ -1,6 +1,6 @@
 import { put, takeEvery, call } from 'redux-saga/effects';
-import { setCharacters } from '../redux/actions';
-import { FETCH_CHARACTERS } from '../redux/type';
+import { setCharacters, setAllCharacters } from '../redux/actions';
+import { FETCH_CHARACTERS, FETCH_ALL_CHARACTERS } from '../redux/type';
 
 async function fetchCharactersFromApi(url: any) {
     const request: any = await fetch(url);
@@ -8,8 +8,15 @@ async function fetchCharactersFromApi(url: any) {
     return data;
 }
 
+async function fetchAllCharactersFromApi(num: number) {
+    const request: any = await fetch(`https://rickandmortyapi.com/api/character/?page=${num}`);
+    const data: any = await request.json();
+    return data;
+}
+
 export function* charactersWatcher(): any {
     yield takeEvery(FETCH_CHARACTERS, charactersWorker);
+    yield takeEvery(FETCH_ALL_CHARACTERS, allCharactersWorker);
 };
 
 export function* charactersWorker(args: any): any {
@@ -18,4 +25,14 @@ export function* charactersWorker(args: any): any {
         data.push(yield call(fetchCharactersFromApi, args.payload[i]));
     }
     yield put(setCharacters(data));
+}
+
+export function* allCharactersWorker(): any {
+    let data: any = yield call(fetchAllCharactersFromApi, 0); // grab the max number of pages
+    let arr: any = [];
+    for (let i = 1; i <= data.info.pages; i++) {
+        data = yield call(fetchAllCharactersFromApi, i);
+        arr = [...arr, ...data.results];
+    }
+    yield put(setAllCharacters(arr));
 }
--- src/Store/redux/charactersReducer.tsx
+++ src/Store/redux/charactersReducer.tsx
@@ -1,7 +1,8 @@
-import { SET_CHARACTERS, RESET_DATA } from "./type";
+import { SET_CHARACTERS, RESET_DATA, SET_ALL_CHARACTERS } from "./type";
 
 const initialState: any = {
     characters: [],
+    allCharacters: [],
     isFetching: false,
 };
 
@@ -15,6 +16,14 @@ export default function charactersReducer(state = initialState, action: any) {
                         ...action.payload
                     ],
                 isFetching: true,
+            }
+        case SET_ALL_CHARACTERS:
+            return {
+                ...state,
+                allCharacters:
+                    [
+                        ...action.payload
+                    ],
             }
         case RESET_DATA:
             return {
