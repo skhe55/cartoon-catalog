af24afe8e8695fc64f6fdc9aa76e9e7a3467af97
skhe55 <flashback2019@mail.ru>
2021-10-16 01:04:12 +0700

    added new acitons

--- src/Store/redux/actions.tsx
+++ src/Store/redux/actions.tsx
@@ -1,4 +1,4 @@
-import { FETCH_SERIES, SET_SERIES, SET_CHARACTERS, RESET_DATA } from "./type"
+import { FETCH_SERIES, SET_SERIES, SET_CHARACTERS, RESET_DATA, SET_ALL_CHARACTERS, SET_LOCATIONS } from "./type"
 
 export const fetchSeries = () => {
     return ({ type: FETCH_SERIES });
@@ -12,6 +12,14 @@ export const setCharacters = (payload: any) => {
     return ({ type: SET_CHARACTERS, payload });
 };
 
+export const setAllCharacters = (payload: any) => {
+    return ({ type: SET_ALL_CHARACTERS, payload });
+};
+
 export const resetData = () => {
     return ({ type: RESET_DATA });
+};
+
+export const setLocations = (payload: any) => {
+    return ({ type: SET_LOCATIONS, payload });
 };
