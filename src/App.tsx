43c39da60aa27d76a470ee49d3edd73e0753a8cd
skhe55 <flashback2019@mail.ru>
2021-10-16 01:06:57 +0700

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { store } from './Store';
 import Main from './Components/Main';
 import AboutEpisode from './Components/AboutEpisode';
+import { AboutCharacter } from './Components/AboutCharacter';
+import { AboutLocation } from './Components/AboutLocation';
 import Navbar from './Components/Navbar/Navbar';
 import 'styles/global.scss';
 
@@ -16,6 +18,8 @@ export const App: React.FC<{}> = () => {
                 <Switch>
                     <Route exact path="/" component={Main} />
                     <Route exact path="/aboutepisode" component={AboutEpisode} />
+                    <Route exact path="/character" component={AboutCharacter} />
+                    <Route exact path="/location" component={AboutLocation} />
                 </Switch>
             </Router>
         </Provider>
--- src/Store/redux-saga/index.tsx
+++ src/Store/redux-saga/index.tsx
@@ -1,10 +1,12 @@
 import { all, fork } from "redux-saga/effects";
 import { seriesWatcher } from "./seriesSaga";
 import { charactersWatcher } from "./charactersSaga";
+import { locationsWatcher } from "./locationSaga";
 
 export function* rootWatcher() {
     yield all([
         fork(seriesWatcher),
         fork(charactersWatcher),
+        fork(locationsWatcher),
     ]);
 };
