91594b8e01037cd3af55ddcf2349fc86160af71c
skhe55 <flashback2019@mail.ru>
2021-10-17 22:39:24 +0700

    delete boilerplate & rewrite worker for fetching data page by page

--- src/Store/redux-saga/seriesSaga.tsx
+++ src/Store/redux-saga/seriesSaga.tsx
@@ -3,21 +3,23 @@ import { FETCH_SERIES } from '../redux/type';
 import { setSeries } from '../redux/actions';
 
 async function fetchSeriesFromApi(num: number) {
-    const request: any = await fetch(`https://rickandmortyapi.com/api/episode?page=${num}`);
-    const data: any = await request.json();
-    return data;
+    try {
+        const request: any = await fetch(`https://rickandmortyapi.com/api/episode?page=${num}`);
+        const data: any = await request.json();
+        return data;
+    } catch (e) {
+        console.log('fetchSeriesFromApi exception: ', e);
+    }
+
 }
 
 export function* seriesWatcher(): any {
     yield takeEvery(FETCH_SERIES, seriesWorker);
 }
 
-export function* seriesWorker(): any {
-    let data: any = yield call(fetchSeriesFromApi, 0); // grab the max number of pages
+export function* seriesWorker(args: any): any {
+    let data: any = yield call(fetchSeriesFromApi, args.payload);
     let arr: any = [];
-    for (let i = 1; i <= data.info.pages; i++) {
-        data = yield call(fetchSeriesFromApi, i);
-        arr = [...arr, ...data.results];
-    }
+    arr = [...data.results];
     yield put(setSeries(arr));
 }
