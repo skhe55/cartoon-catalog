16f37a696bf79f8c917df13c7071e93f10b6c47d
skhe55 <flashback2019@mail.ru>
2021-10-17 22:22:48 +0700

    add router for locations page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './Store';
@@ -6,6 +6,7 @@ import Main from './Components/Main';
 import AboutEpisode from './Components/AboutEpisode';
 import { AboutCharacter } from './Components/AboutCharacter';
 import { AboutLocation } from './Components/AboutLocation';
+import { CharacterCard } from './Components/LocationCards/CharacterCard';
 import Navbar from './Components/Navbar/Navbar';
 import 'styles/global.scss';
 
@@ -20,6 +21,7 @@ export const App: React.FC<{}> = () => {
                     <Route exact path="/aboutepisode" component={AboutEpisode} />
                     <Route exact path="/character" component={AboutCharacter} />
                     <Route exact path="/location" component={AboutLocation} />
+                    <Route exact path="/aboutlocation" component={CharacterCard} />
                 </Switch>
             </Router>
         </Provider>
