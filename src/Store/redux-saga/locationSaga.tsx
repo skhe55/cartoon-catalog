7923e2452ffcb943828541fdf01c78bcbb193491
skhe55 <flashback2019@mail.ru>
2021-10-17 22:40:00 +0700

    page by page fetching data added

--- src/Store/redux-saga/locationSaga.tsx
+++ src/Store/redux-saga/locationSaga.tsx
@@ -3,21 +3,26 @@ import { FETCH_LOCATIONS } from '../redux/type';
 import { setLocations } from '../redux/actions';
 
 async function fetchLocationsFromApi(num: number) {
-    const request: any = await fetch(`https://rickandmortyapi.com/api/location?page=${num}`);
-    const data: any = await request.json();
-    return data;
+    try {
+        const request: any = await fetch(`https://rickandmortyapi.com/api/location?page=${num}`);
+        const data: any = await request.json();
+        return data;
+    } catch (e) {
+        console.log(e);
+    }
+
 }
 
 export function* locationsWatcher(): any {
-    yield takeEvery(FETCH_LOCATIONS, locationsWorker);
+    yield takeLatest(FETCH_LOCATIONS, locationsWorker);
 }
 
-export function* locationsWorker(): any {
-    let data: any = yield call(fetchLocationsFromApi, 0); // grab the max number of pages
+
+export function* locationsWorker(args: any): any {
+    let data: any = [];
     let arr: any = [];
-    for (let i = 1; i <= data.info.pages; i++) {
-        data = yield call(fetchLocationsFromApi, i);
-        arr = [...arr, ...data.results];
-    }
+    data = yield call(fetchLocationsFromApi, args.payload);
+    arr = [...data.results];
     yield put(setLocations(arr));
 }
+
