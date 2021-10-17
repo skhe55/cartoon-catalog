a85fc964158e3d0017916de58f341e44a5baf736
skhe55 <flashback2019@mail.ru>
2021-10-17 22:26:10 +0700

    added infinity scroll for Location page

--- src/Components/AboutLocation.tsx
+++ src/Components/AboutLocation.tsx
@@ -1,30 +1,69 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../Store/redux';
-import { FETCH_ALL_CHARACTERS, FETCH_LOCATIONS } from '../Store/redux/type';
+import { FETCH_LOCATIONS, SET_FETCHISUP } from '../Store/redux/type';
 import '../styles/AboutLocation.scss';
-
+import InfoLocationCard from './LocationCards/InfoLocationCard';
 export const AboutLocation = () => {
-    const characters: any = useSelector((state: RootState) => state.charactersReducer.allCharacters);
+    const locationsIsFetch: boolean = useSelector((state: RootState) => state.locationsReducer.locationsIsFetch);
     const locations: any = useSelector((state: RootState) => state.locationsReducer.locations);
+    const totalCount: any = useSelector((state: RootState) => state.locationsReducer.totalCount);
+    const countPages: number = 6;
     const dispatch = useDispatch();
+    const [searchValue, setSearchValue] = useState<string>("");
+    const getLocation = (e: string, array: any) => {
+        return array.filter((el: any) => el.name.includes(e));
+    }
 
-    const getResidents = (array: Array<object>, el: string) => {
-        return array.filter((e: any) => e.id == el)
+    const scrollHandler = (e: any) => {
+        const a = e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight);
+        if (a < 100 && (totalCount <= countPages)) {
+            dispatch({ type: SET_FETCHISUP })
+        }
     }
 
     useEffect(() => {
-        dispatch({ type: FETCH_ALL_CHARACTERS })
-        dispatch({ type: FETCH_LOCATIONS })
-    }, [])
-    console.log(locations);
-    console.log(characters);
+        if (locationsIsFetch) {
+            dispatch({ type: FETCH_LOCATIONS, payload: totalCount });
+        }
+    }, [locationsIsFetch])
+
+    useEffect(() => {
+        document.addEventListener('scroll', scrollHandler);
+        return function () {
+            document.removeEventListener('scroll', scrollHandler);
+        }
+    }, [locationsIsFetch])
+
     return (
         <div className="mainBody">
-            <div className="imgFlexContainer">
-                <div className="wrapImgContainer"></div>
+            <div className="inputFlexContainer">
+                <input onChange={(e) => setSearchValue(e.target.value)} className="_inputLocation" placeholder="Type here name of location (Earth or etc.)"></input>
             </div>
-            <div className="cardFlexContainer"></div>
+            <div className="__bodyFlex">
+                {searchValue == "" ?
+                    locations.map((e: any) => {
+                        return (
+                            <InfoLocationCard
+                                name={e.name}
+                                dimension={e.dimension}
+                                residents={e.residents}
+                                type={e.type}
+                            />
+                        );
+                    }) :
+                    getLocation(searchValue, locations).map((e: any) => {
+                        return (
+                            <InfoLocationCard
+                                name={e.name}
+                                dimension={e.dimension}
+                                residents={e.residents}
+                                type={e.type}
+                            />
+                        );
+                    })
+                }
+            </div>
         </div>
     )
 }
